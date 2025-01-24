9159f78e4a5903ad0cfebe84ac47428397a7b72e
mirae <minmimail@gmail.com>
2025-01-24 10:17:58 +0900

    Feat: 반응형 작업 추가

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(function(){
-    // Header
+    $("html, body").animate({ scrollTop: 0 }, 500); 
+
+    //Header
     var lastScrollTop = 0;
     
     $(window).scroll(function(e){
@@ -17,10 +19,37 @@ $(function(){
         lastScrollTop = st;
     });
 
+    //Moblie Menu
+    $(".gnb-mobile-trigger").click(function() {
+        $(this).toggleClass("active");
+        $("body").toggleClass("scroll-lock");
+        $(".header").toggleClass("open");
+        $(".gnb-mobile").toggleClass("open");
 
-    gsap.registerPlugin(ScrollTrigger);
+        if($(this).hasClass("active")){
+            const gnbMo = gsap.timeline({});
+            gnbMo.fromTo(".gnb-mobile__inner",{autoAlpha:0},{autoAlpha:1, duration: 0.2, ease:'power1.inOut'}, 0)
+            gnbMo.fromTo(".gnb-mobile__item", {y:10, autoAlpha: 0}, {y:0, autoAlpha: 1, stagger:0.1})
+            gnbMo.fromTo(".gnb-mobile__btn", {autoAlpha: 0}, {autoAlpha: 1}, "-=0.3")
+        } else {
+            $(".gnb-mobile__item--with-submenu").removeClass("active")
+            gsap.to(".toggle-arrow",{rotateZ:0})
+        }
+    });
 
-    // Section BG/MENU
+    $(".gnb-mobile__item--with-submenu").click(function() {
+        $(this).toggleClass("active");
+
+        if($(this).hasClass("active")){
+            TweenMax.fromTo(".gnb-mobile__submenu li",{autoAlpha:0},{autoAlpha:1, stagger:0.1, duration: 0.5, ease:'power1.inOut'})
+            TweenMax.to(".toggle-arrow",{rotateZ:180})
+        } else {
+            TweenMax.to(".toggle-arrow",{rotateZ:0})
+        } 
+    });
+
+
+    // Section BG
     gsap.utils.toArray(".sec").forEach((sec) => {
         ScrollTrigger.create({
             trigger: sec,
@@ -40,6 +69,8 @@ $(function(){
     });
 
 
+    gsap.registerPlugin(ScrollTrigger);
+
     // Text slide up
     gsap.utils.toArray(".sec:not(.intro) .slide-up").forEach((slideUp) => {
         gsap.to(slideUp.querySelectorAll("span"), {
@@ -47,8 +78,8 @@ $(function(){
                 trigger: slideUp,
                 start: "-=200 center",
                 end: "bottom center",
-                //markers: true,
-                //id: "slideUp",
+                // markers: true,
+                // id: "slideUp",
                 toggleActions: "play none none reverse",
             },
             y: 0,
@@ -60,7 +91,7 @@ $(function(){
     });
 
 
-    // First
+    //First
     const first = gsap.timeline({});
         first.to(".intro .slide-up span",{y:0, autoAlpha:1, ease:'circ.out', stagger: 0.1, duration:1}, 0)
         .to(".intro", {yPercent:-100, height: 0, duration:1}, '+=1')
@@ -91,6 +122,7 @@ $(function(){
             trigger: ".hello",
             start: "-=300 center",
             end: "bottom center",
+            toggleActions: "play none none reverse",
             // id: "helloSplit",
             // markers: true,
         },
@@ -108,6 +140,9 @@ $(function(){
             end:"top bottom",
             // markers: true,
             // id: "helloLeave",
+            onEnter:()=>{
+                gsap.to(".hello__inner", {autoAlpha:"1"})
+            },
             onLeave:()=>{
                 gsap.to(".hello__inner", {autoAlpha:"0"})
             },
@@ -117,6 +152,21 @@ $(function(){
         }
     });
 
+    gsap.to(".exhib", {
+        scrollTrigger:{
+            trigger:".exhib",
+            start:"top center",
+            end:"bottom center",
+            // markers: true,
+            // id: "exhiEnter",
+            onEnter:()=>{
+                gsap.to(".exhib__inner", {autoAlpha:"1"})
+            },
+            onLeaveBack:()=>{
+                gsap.to(".exhib__inner", {autoAlpha:"0"})
+            }
+        }
+    });
 
     // About
     gsap.to(".about__grid", {
@@ -137,38 +187,74 @@ $(function(){
 
 
     //Space
-    const space = gsap.timeline({defaults: {duration: 300},
-        scrollTrigger: {
-            trigger: ".space",
-            start: 'top',
-            end: "bottom bottom",
-            scrub: 1,
-            pin: true,
-            pinSpacing: false,
-            // markers: true,
-            // id: "space",
+    ScrollTrigger.matchMedia({
+        "(min-width: 1024px)":function(){
+            const space = gsap.timeline({defaults: {duration: 300},
+                scrollTrigger: {
+                    trigger: ".space",
+                    start: 'top',
+                    end: "bottom bottom",
+                    scrub: 1,
+                    pin: true,
+                    pinSpacing: false,
+                    // markers: true,
+                    // id: "space",
+                }
+            })
+            space.to(".fade-in__item:nth-child(1)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(1)", { autoAlpha: 0.2 }, "<")
+            .to(".space__title", { yPercent:-100 }, "<")
+        
+            .from(".fade-in__item:nth-child(2)", { autoAlpha: 0 }, "<")
+            .from(".space__pagination-item:nth-child(2)", { autoAlpha: 0.2 }, "<")
+            .to(".fade-in__item:nth-child(2)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(2)", { autoAlpha: 0.2 }, "<")
+            .to(".space__title", { yPercent:-200 }, "<")
+        
+            .from(".fade-in__item:nth-child(3)", { autoAlpha: 0 }, "<")
+            .from(".space__pagination-item:nth-child(3)", { autoAlpha: 0.2 }, "<")
+            .to(".fade-in__item:nth-child(3)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(3)", { autoAlpha: 0.2 }, "<")
+            .to(".space__title", { yPercent:-300 }, "<")
+        
+            .from(".fade-in__item:nth-child(4)", { autoAlpha: 0 }, "<")
+            .from(".space__pagination-item:nth-child(4)", { autoAlpha: 0.2 }, "<")
+            .to(".fade-in__item:nth-child(4)", { autoAlpha: 1, duration: 600 })
+            .to(".space__pagination-item:nth-child(4)", { autoAlpha: 1, duration: 600 }, "<")
+        },
+        "(max-width: 1021px)":function(){
+            const space = gsap.timeline({defaults: {duration: 300},
+                scrollTrigger: {
+                    trigger: ".space",
+                    start: 'top',
+                    end: "bottom bottom",
+                    scrub: 1,
+                    pin: true,
+                    pinSpacing: false,
+                }
+            })
+            space.to(".fade-in__item:nth-child(1)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(1)", { autoAlpha: 0.2, color: "#000", width: "25%" }, "<")
+            .to(".space__title", { yPercent:-100 }, "<")
+        
+            .from(".fade-in__item:nth-child(2)", { autoAlpha: 0 }, "<")
+            .to(".space__pagination-item:nth-child(2)", { autoAlpha: 1, color: "#fff", width: "40vw" }, "<")
+            .to(".fade-in__item:nth-child(2)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(2)", { autoAlpha: 0.2, color: "#000", width: "25%" }, "<")
+            .to(".space__title", { yPercent:-200 }, "<")
+        
+            .from(".fade-in__item:nth-child(3)", { autoAlpha: 0 }, "<")
+            .to(".space__pagination-item:nth-child(3)", { autoAlpha: 1, color: "#fff", width: "40vw" }, "<")
+            .to(".fade-in__item:nth-child(3)", { autoAlpha: 0, delay: 600 })
+            .to(".space__pagination-item:nth-child(3)", { autoAlpha: 0.2, color: "#000", width: "25%" }, "<")
+            .to(".space__title", { yPercent:-300 }, "<")
+        
+            .from(".fade-in__item:nth-child(4)", { autoAlpha: 0 }, "<")
+            .to(".space__pagination-item:nth-child(4)", { autoAlpha: 1, color: "#fff", width: "40vw" }, "<")
+            .to(".fade-in__item:nth-child(4)", { autoAlpha: 1, duration: 600 })
         }
     })
-    space.to(".fade-in__item:nth-child(1)", { autoAlpha: 0, delay: 600 })
-    .to(".space__pagination-item:nth-child(1)", { autoAlpha: 0.2 }, "<")
-    .to(".space__title", { yPercent:-100 }, "<")
-
-    .from(".fade-in__item:nth-child(2)", { autoAlpha: 0 }, "<")
-    .from(".space__pagination-item:nth-child(2)", { autoAlpha: 0.2 }, "<")
-    .to(".fade-in__item:nth-child(2)", { autoAlpha: 0, delay: 600 })
-    .to(".space__pagination-item:nth-child(2)", { autoAlpha: 0.2 }, "<")
-    .to(".space__title", { yPercent:-200 }, "<")
-
-    .from(".fade-in__item:nth-child(3)", { autoAlpha: 0 }, "<")
-    .from(".space__pagination-item:nth-child(3)", { autoAlpha: 0.2 }, "<")
-    .to(".fade-in__item:nth-child(3)", { autoAlpha: 0, delay: 600 })
-    .to(".space__pagination-item:nth-child(3)", { autoAlpha: 0.2 }, "<")
-    .to(".space__title", { yPercent:-300 }, "<")
-
-    .from(".fade-in__item:nth-child(4)", { autoAlpha: 0 }, "<")
-    .from(".space__pagination-item:nth-child(4)", { autoAlpha: 0.2 }, "<")
-    .to(".fade-in__item:nth-child(4)", { autoAlpha: 1, duration: 600 })
-    .to(".space__pagination-item:nth-child(4)", { autoAlpha: 1, duration: 600 }, "<")
+    
 
 
     gsap.to(".space", {
@@ -176,8 +262,8 @@ $(function(){
             trigger:".visit",
             start:"-15% top",
             end:"bottom top",
-            markers: true,
-            id: "spaceLeave",
+            // markers: true,
+            // id: "spaceLeave",
             onEnter:()=>{
                 gsap.to(".space__inner", {autoAlpha:"0"})
             },
@@ -187,11 +273,12 @@ $(function(){
         }
     });
 
+
     //  Visit
     const card = gsap.timeline({ 
         scrollTrigger: { 
             trigger: ".visit__card-wrap",
-            start: "top top",
+            start: "center center",
             //anticipatePin: window.innerWidth < 768 ? 1 : 0,
             anticipatePin: 1,
             end: () => '+=' + window.innerHeight * 1,
@@ -229,7 +316,6 @@ $(function(){
     });
     faq.to(".faq__item",{"--active-scale":1, autoAlpha: 1, stagger:0.1, duration: 0.5})
     .to(".faq__question",{autoAlpha: 1, stagger:0.1}, "-=0.4")
-   
 
 
     window.addEventListener("resize", ScrollTrigger.update);
